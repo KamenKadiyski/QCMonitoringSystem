1a66e5af810eadd6d3f7af1c8fe59c4f3c6a5479
kamenkadiyski <kamen.kadiyski@gmail.com>
2026-03-25 20:30:42 +0000

    Commit changes to add job and equipment data

--- equipment/serializers.py
+++ equipment/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from equipment.models import Machine, Tool
+from equipment.signals import tool_is_compatible
 
 class FileUploadSerializer(serializers.Serializer):
     file = serializers.FileField()
@@ -11,6 +12,36 @@ class MachineSerializer(serializers.ModelSerializer):
     class Meta:
         model = Machine
         fields = '__all__'
+
+    def validate(self, attrs):
+        compatible_tools = attrs.get('compatible_tools')
+        if compatible_tools is None:
+            return attrs
+
+        if self.instance is not None:
+            machine = Machine(
+                max_clamping_force=attrs.get('max_clamping_force', self.instance.max_clamping_force),
+                max_tool_width=attrs.get('max_tool_width', self.instance.max_tool_width),
+                max_tool_height=attrs.get('max_tool_height', self.instance.max_tool_height),
+                max_tool_thickness=attrs.get('max_tool_thickness', self.instance.max_tool_thickness),
+                max_moving_platen_stroke=attrs.get('max_moving_platen_stroke', self.instance.max_moving_platen_stroke),
+                max_injection_capacity=attrs.get('max_injection_capacity', self.instance.max_injection_capacity),
+                max_ejecting_stroke=attrs.get('max_ejecting_stroke', self.instance.max_ejecting_stroke),
+                number_of_ejector_cores=attrs.get('number_of_ejector_cores', self.instance.number_of_ejector_cores),
+            )
+        else:
+            machine = Machine(
+                max_clamping_force=attrs['max_clamping_force'],
+                max_tool_width=attrs['max_tool_width'],
+                max_tool_height=attrs['max_tool_height'],
+                max_tool_thickness=attrs['max_tool_thickness'],
+                max_moving_platen_stroke=attrs['max_moving_platen_stroke'],
+                max_injection_capacity=attrs['max_injection_capacity'],
+                max_ejecting_stroke=attrs['max_ejecting_stroke'],
+                number_of_ejector_cores=attrs['number_of_ejector_cores'],
+            )
+        attrs['compatible_tools'] = [tool for tool in compatible_tools if tool_is_compatible(tool, machine)]
+        return attrs
 
     def create(self,validated_data):
         compatible_tools_data = validated_data.pop('compatible_tools', [])
@@ -38,8 +69,6 @@ class ToolSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tool
         fields = '__all__'
-
-
 
 
 
--- equipment/signals.py
+++ equipment/signals.py
@@ -1,78 +1,62 @@
-from django.db.models.signals import post_save, m2m_changed
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from QCMonitoringSystem.settings import TOOL_TOLERANCE
 from .models import Tool, Machine
 from django.db.models import Q
-def tool_is_compatible(tool, machine, tolerance=TOOL_TOLERANCE):
-    return (
-        tool.clamping_force >= machine.max_clamping_force * tolerance and
-        tool.tool_width >= machine.max_tool_width * tolerance and
-        tool.tool_height >= machine.max_tool_height * tolerance and
-        tool.tool_thickness >= machine.max_tool_thickness * tolerance and
-        tool.moving_platen_stroke >= machine.max_moving_platen_stroke * tolerance and
-        tool.injection_capacity >= machine.max_injection_capacity * tolerance and
-        tool.ejecting_stroke >= machine.max_ejecting_stroke * tolerance and
-        tool.number_of_ejector_cores >= machine.number_of_ejector_cores * tolerance
-    )
+import logging
 
-def compatible_tool_for_machine(machine,tolerance=TOOL_TOLERANCE):
+logger = logging.getLogger(__name__)
+
+THICKNESS_MIN_RATIO = 0.30
+
+
+def tool_is_compatible(tool, machine, tolerance=TOOL_TOLERANCE):
+    is_compatible = (
+        tool.clamping_force <= machine.max_clamping_force * tolerance and
+        tool.tool_width <= machine.max_tool_width * tolerance and
+        tool.tool_height <= machine.max_tool_height * tolerance and
+        machine.max_tool_thickness * THICKNESS_MIN_RATIO <= tool.tool_thickness <= machine.max_tool_thickness and
+        tool.moving_platen_stroke <= machine.max_moving_platen_stroke * tolerance and
+        tool.injection_capacity <= machine.max_injection_capacity * tolerance and
+        tool.ejecting_stroke <= machine.max_ejecting_stroke * tolerance and
+        tool.number_of_ejector_cores <= machine.number_of_ejector_cores
+    )
+    if not is_compatible:
+        logger.debug(f"Tool {tool.code} NOT compatible with {machine.machine_number}")
+    return is_compatible
+
+def compatible_tool_for_machine(machine, tolerance=TOOL_TOLERANCE):
     compatible_tools = Tool.objects.filter(
-        Q(clamping_force__gte=machine.max_clamping_force * tolerance) &
-        Q(tool_width__gte=machine.max_tool_width * tolerance)&
-        Q(tool_height__gte=machine.max_tool_height * tolerance)&
-        Q(tool_thickness__lte=machine.max_tool_thickness * tolerance) &
-        Q(moving_platen_stroke__gte=machine.max_moving_platen_stroke * tolerance) &
-        Q(injection_capacity__gte=machine.max_injection_capacity * tolerance) &
-        Q(ejecting_stroke__gte=machine.max_ejecting_stroke * tolerance) &
-        Q(number_of_ejector_cores__gte=machine.number_of_ejector_cores * tolerance)
+        Q(clamping_force__lte=machine.max_clamping_force * tolerance) &
+        Q(tool_width__lte=machine.max_tool_width * tolerance) &
+        Q(tool_height__lte=machine.max_tool_height * tolerance) &
+        Q(tool_thickness__gte=machine.max_tool_thickness * THICKNESS_MIN_RATIO) &
+        Q(tool_thickness__lte=machine.max_tool_thickness) &
+        Q(moving_platen_stroke__lte=machine.max_moving_platen_stroke * tolerance) &
+        Q(injection_capacity__lte=machine.max_injection_capacity * tolerance) &
+        Q(ejecting_stroke__lte=machine.max_ejecting_stroke * tolerance) &
+        Q(number_of_ejector_cores__lte=machine.number_of_ejector_cores)
     )
     return compatible_tools
 
 @receiver(post_save, sender=Machine)
 def add_compatible_tools_on_machine_create(sender, instance, created, **kwargs):
-    if not created:
-        return
-
-    compatible_tools = compatible_tool_for_machine(instance)
-    if compatible_tools.exists():
-        instance.compatible_tools.add(*compatible_tools)
-
-
-@receiver(m2m_changed, sender=Machine.compatible_tools.through)
-def validate_tool_compatibility(sender, instance, action, reverse, pk_set, **kwargs):
-    if action!= "pre_add":
-        return
-
-    tools = Tool.objects.filter(id__in=pk_set)
-    valid = [
-        tool.pk for tool in tools if tool_is_compatible(tool, instance)
-    ]
-    pk_set.clear()
-    pk_set.update(valid)
-
+    if created:
+        compatible_tools = compatible_tool_for_machine(instance)
+        instance.compatible_tools.set(compatible_tools)
 
 
 @receiver(post_save, sender=Tool)
 def add_tool_to_compatible_machines(sender, instance, created, **kwargs):
-    if not created:
-        return
-
-    compatible_machines = Machine.objects.filter(
-        Q(max_clamping_force__gte=instance.clamping_force / TOOL_TOLERANCE) &
-        Q(max_tool_width__gte=instance.tool_width / TOOL_TOLERANCE) &
-        Q(max_tool_height__gte=instance.tool_height / TOOL_TOLERANCE) &
-        Q(max_tool_thickness__lte=instance.tool_thickness / TOOL_TOLERANCE) &
-        Q(max_moving_platen_stroke__gte=instance.moving_platen_stroke / TOOL_TOLERANCE) &
-        Q(max_injection_capacity__gte=instance.injection_capacity / TOOL_TOLERANCE) &
-        Q(max_ejecting_stroke__gte=instance.ejecting_stroke / TOOL_TOLERANCE) &
-        Q(number_of_ejector_cores__gte=instance.number_of_ejector_cores)
-    )
-
-    for machine in compatible_machines:
-        machine.compatible_tools.add(instance)
-
-
-
-
-
+    compatible_machines = []
+    # Check compatibility against all machines
+    for machine in Machine.objects.all():
+        if tool_is_compatible(instance, machine):
+            compatible_machines.append(machine)
+    
+    # Update the ManyToMany relationship
+    if compatible_machines:
+        instance.compatible_machines.set(compatible_machines)
+    else:
+        logger.info(f"No compatible machines found for tool {instance.code}")

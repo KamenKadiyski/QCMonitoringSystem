ef6e5d0a3e28272bce35ff5bd77c5eae1b3c141c
kamenkadiyski <kamen.kadiyski@gmail.com>
2026-04-02 16:18:42 +0300

    Commit backend and frontend together

--- equipment/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class EquipmentConfig(AppConfig):
-    name = 'equipment'
-
-
-    def ready(self):
-        import equipment.signals
--- qcloging/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QclogingConfig(AppConfig):
-    name = 'qcloging'

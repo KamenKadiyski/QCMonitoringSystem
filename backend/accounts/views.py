abcce2a5f779f7b81e1d4bea969cba026d03e6f8
kamenkadiyski <kamen.kadiyski@gmail.com>
2026-04-03 16:34:22 +0300

    Commit backend and frontend together

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -42,12 +42,12 @@ def home_view(request):
             menu_items.extend([
                 {'title': 'QC Logging', 'url': reverse('qcloging:list_qc_logs'), 'icon': 'journal-check', 'color': 'text-primary'},
                 {'title': 'Jobs', 'url': reverse('jobs:list_jobs'), 'icon': 'briefcase', 'color': 'text-primary'},
-                {'title': 'Trading Parties', 'url': None, 'icon': 'building', 'color': 'text-primary'},
+                {'title': 'Trading Parties', 'url': reverse('traidingparties:add_supplier'), 'icon': 'building', 'color': 'text-primary'},
                 {'title': 'Accounts (HR)', 'url': 'http://localhost:5173', 'icon': 'person-gear', 'color': 'text-info'},
-                {'title': 'Materials', 'url': None, 'icon': 'box-seam', 'color': 'text-warning'},
+                {'title': 'Materials', 'url': reverse('materials:list_materials'), 'icon': 'box-seam', 'color': 'text-warning'},
                 {'title': 'Tools', 'url': reverse('equipment:tool-list'), 'icon': 'tools', 'color': 'text-warning'},
                 {'title': 'Machine', 'url': reverse('equipment:machine-list'), 'icon': 'machines', 'color': 'text-warning'},
-                {'title': 'Създай QC Issue', 'url': None, 'icon': 'exclamation-octagon', 'color': 'text-danger'},
+                {'title': 'Създай QC Issue', 'url': reverse('qcloging:add_qc_issue'), 'icon': 'exclamation-octagon', 'color': 'text-danger'},
                 {'title': 'Job Log', 'url': reverse('jobs:list_jobs_logs'), 'icon': 'clipboard-data', 'color': 'text-secondary'},
                 {'title': 'Scrap Reason', 'url': reverse('jobs:add_scrap_reason'), 'icon': 'clipboard-data','color': 'text-secondary'},
             ])
@@ -59,9 +59,9 @@ def home_view(request):
             menu_items.extend([
                 {'title': 'QC Logging', 'url': reverse('qcloging:list_qc_logs'), 'icon': 'journal-check', 'color': 'text-primary'},
                 {'title': 'Jobs', 'url': reverse('jobs:list_jobs'), 'icon': 'briefcase', 'color': 'text-primary'},
-                {'title': 'Trading Parties', 'url': None, 'icon': 'building', 'color': 'text-primary'},
-                {'title': 'Scrap Reason', 'url': reverse('jobs:add_scrap_reason'), 'icon': 'clipboard-data',
-                 'color': 'text-secondary'},
+                {'title': 'Trading Parties', 'url': reverse('traidingparties:add_supplier'), 'icon': 'building', 'color': 'text-primary'},
+                {'title': 'Scrap Reason', 'url': reverse('jobs:add_scrap_reason'), 'icon': 'clipboard-data','color': 'text-secondary'},
+                {'title': 'Materials', 'url': reverse('materials:list_materials'), 'icon': 'box-seam','color': 'text-warning'},
             ])
 
 
@@ -74,15 +74,17 @@ def home_view(request):
         # 4. PRODUCTION MANAGER (Materials & Equipment)
         if any(group.lower() == 'production manager' for group in user_groups):
             menu_items.extend([
-                {'title': 'Materials', 'url': None, 'icon': 'box-seam', 'color': 'text-warning'},
-                {'title': 'Equipment', 'url': None, 'icon': 'tools', 'color': 'text-warning'},
+                {'title': 'Materials', 'url': reverse('materials:list_materials'), 'icon': 'box-seam', 'color': 'text-warning'},
+                {'title': 'Tools', 'url': reverse('equipment:tool-list'), 'icon': 'tools', 'color': 'text-warning'},
+                {'title': 'Machine', 'url': reverse('equipment:machine-list'), 'icon': 'machines','color': 'text-warning'},
+                {'title': 'Trading Parties', 'url': reverse('traidingparties:add_supplier'), 'icon': 'building','color': 'text-primary'},
             ])
 
         # 5. SUPERVISOR, QC INSPECTOR, TEAM LEADER (QC Issue Creation)
         special_roles = ['Supervisor', 'Supervisors', 'QC Inspector', 'Team Leader']
         if any(role in user_groups for role in special_roles):
             menu_items.extend([
-                {'title': 'Създай QC Issue', 'url': None, 'icon': 'exclamation-octagon', 'color': 'text-danger'},
+                {'title': 'Създай QC Issue', 'url': reverse('qcloging:add_qc_issue'), 'icon': 'exclamation-octagon', 'color': 'text-danger'},
                 {'title': 'Job Log', 'url': reverse('jobs:list_jobs_logs'), 'icon': 'clipboard-data', 'color': 'text-secondary'},
                 {'title': 'QC Logging', 'url': reverse('qcloging:list_qc_logs'), 'icon': 'journal-check','color': 'text-primary'},
             ])
--- backend/qcloging/urls.py
+++ backend/qcloging/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from django.views import View
 
-from .views import AddToQCLogView, JobLogListView, UpdateQCLogView
+from .views import AddToQCLogView, JobLogListView, UpdateQCLogView, QCIssueListView, AddToQCIssueView, UpdateQCIssueView
 
 app_name = 'qcloging'
 
@@ -8,6 +9,9 @@ urlpatterns = [
     path('', JobLogListView.as_view(), name='list_qc_logs'),
     path('add_to_qc_log/', AddToQCLogView.as_view(), name='add_qc_log'),
     path('qc/update/<int:pk>/', UpdateQCLogView.as_view(), name='update_qc_log'),
+    path('qc-issue/',QCIssueListView.as_view(),name='list_qc_issues'),
+    path('add_to_qc_issue/',AddToQCIssueView.as_view(),name='add_qc_issue'),
+    path('qc-issue/update/<int:pk>/',UpdateQCIssueView.as_view(),name='update_qc_issue'),
 
 
 
